3f15ca05b8c2d629f3f4aaaa4543c713d7d234ff
vancinad <vancinad@yahoo.com>
2026-02-14 17:53:39 -0600

    docs: update Atlas env var names in README

--- README.md
+++ README.md
@@ -5,6 +5,27 @@ Lichess Tournament Director
 
 **Goal:** Create a system for automatically managing long-running Swiss tournaments on Lichess.org. 
 
+## Development
+
+### Run the service
+
+```bash
+sbt run
+```
+
+Set your Atlas connection settings before starting the app:
+
+```bash
+export LITD_DB_CONNECT_STRING='mongodb+srv://<user>:<password>@<cluster>/?retryWrites=true&w=majority'
+export LITD_DB_DBNAME='litd'
+```
+
+The health endpoint should respond with `ok`:
+
+```bash
+curl http://localhost:8080/health
+```
+
 References:
 * [Lichess Feedback - Clocks start automatically in Swiss Tournament?!](https://lichess.org/forum/redirect/post/L9boi7eP)
 * [Lichess Feedback - starting timer should be removed#3](https://lichess.org/forum/redirect/post/AvLmQzeD)
---
+++ src/main/scala/litd/Main.scala
@@ -0,0 +1,66 @@
+package litd
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives.*
+import com.typesafe.config.{Config, ConfigFactory}
+import org.mongodb.scala.MongoClient
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+
+final case class HttpConfig(host: String, port: Int)
+final case class MongoConfig(uri: String, database: String)
+final case class AppConfig(http: HttpConfig, mongodb: MongoConfig)
+
+object AppConfigLoader {
+  def load(config: Config = ConfigFactory.load()): AppConfig = {
+    val httpConfig = config.getConfig("litd.http")
+    val mongoConfig = config.getConfig("litd.mongodb")
+    AppConfig(
+      http = HttpConfig(
+        host = httpConfig.getString("host"),
+        port = httpConfig.getInt("port")
+      ),
+      mongodb = MongoConfig(
+        uri = mongoConfig.getString("uri"),
+        database = mongoConfig.getString("database")
+      )
+    )
+  }
+}
+
+object MongoClientFactory {
+  def create(mongoConfig: MongoConfig): MongoClient = MongoClient(mongoConfig.uri)
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "litd")
+    implicit val ec: ExecutionContext = system.executionContext
+
+    val appConfig = AppConfigLoader.load()
+    val mongoClient = MongoClientFactory.create(appConfig.mongodb)
+
+    // GET /health -> "ok"
+    val routes = path("health") {
+      get {
+        complete(StatusCodes.OK -> "ok")
+      }
+    }
+
+    Http()
+      .newServerAt(appConfig.http.host, appConfig.http.port)
+      .bind(routes)
+      .onComplete {
+        case Success(binding) =>
+          system.log.info("Server running at {}", binding.localAddress)
+        case Failure(ex) =>
+          system.log.error("Failed to bind HTTP endpoint", ex)
+          mongoClient.close()
+          system.terminate()
+      }
+  }
+}
